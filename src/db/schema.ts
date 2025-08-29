630b8ed476e110f9cf969933892f4c5da96d970b
heatyux <421743974@qq.com>
2025-08-29 14:59:43 +0800

    feat: drizzle setup

---
+++ src/db/schema.ts
@@ -0,0 +1,22 @@
+import {
+  pgTable,
+  text,
+  timestamp,
+  uniqueIndex,
+  uuid,
+  varchar,
+} from 'drizzle-orm/pg-core'
+
+export const users = pgTable(
+  'users',
+  {
+    id: uuid('id').defaultRandom().primaryKey(),
+    clerkId: varchar('clerk_id').unique().notNull(),
+    name: text('name').notNull(),
+    // TODO: add banner fields
+    imageUrl: text('image_url').notNull(),
+    createdAt: timestamp('created_at').defaultNow().notNull(),
+    updatedAt: timestamp('updated_at').defaultNow().notNull(),
+  },
+  (t) => [uniqueIndex('clerk_id_idx').on(t.clerkId)],
+)
