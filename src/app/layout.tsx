2a77fed9a39425ebf41f10c69e84f745db7565ff
heatyux <421743974@qq.com>
2025-08-29 12:05:43 +0800

    feat: clerk setup

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,3 +1,4 @@
+import { ClerkProvider } from '@clerk/nextjs'
 import type { Metadata } from 'next'
 import { Inter } from 'next/font/google'
 
@@ -18,8 +19,10 @@ export default function RootLayout({
   children: React.ReactNode
 }>) {
   return (
-    <html lang="en">
-      <body className={`${inter.className} antialiased`}>{children}</body>
-    </html>
+    <ClerkProvider afterSignOutUrl={'/'}>
+      <html lang="en">
+        <body className={`${inter.className} antialiased`}>{children}</body>
+      </html>
+    </ClerkProvider>
   )
 }
--- src/modules/auth/ui/components/auth-button.tsx
+++ src/modules/auth/ui/components/auth-button.tsx
@@ -1,3 +1,6 @@
+'use client'
+
+import { SignInButton, SignedIn, SignedOut, UserButton } from '@clerk/nextjs'
 import { UserCircleIcon } from 'lucide-react'
 
 import { Button } from '@/components/ui/button'
@@ -6,11 +9,22 @@ export const AuthButton = () => {
   // TODO: add different auth states
 
   return (
-    <Button
-      variant="outline"
-      className="rounded-full border-blue-500/20 px-4 py-2 text-sm font-medium text-blue-600 shadow-none hover:text-blue-500"
-    >
-      <UserCircleIcon className="size-5" /> Sign In
-    </Button>
+    <>
+      <SignedOut>
+        <SignInButton mode="modal">
+          <Button
+            variant="outline"
+            className="rounded-full border-blue-500/20 px-4 py-2 text-sm font-medium text-blue-600 shadow-none hover:text-blue-500"
+          >
+            <UserCircleIcon className="size-5" /> Sign In
+          </Button>
+        </SignInButton>
+      </SignedOut>
+
+      <SignedIn>
+        <UserButton />
+        {/* TODO: Add menu items for studio and user profile */}
+      </SignedIn>
+    </>
   )
 }
