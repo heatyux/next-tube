bef1ee96b97639b5863f94505ce93402f5ccf731
heatyux <421743974@qq.com>
2025-09-02 11:09:26 +0800

    feat: trpc basic setup
    trpc client setup

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -2,6 +2,8 @@ import { ClerkProvider } from '@clerk/nextjs'
 import type { Metadata } from 'next'
 import { Inter } from 'next/font/google'
 
+import { TRPCProvider } from '@/trpc/client'
+
 import './globals.css'
 
 const inter = Inter({
@@ -21,7 +23,9 @@ export default function RootLayout({
   return (
     <ClerkProvider afterSignOutUrl={'/'}>
       <html lang="en">
-        <body className={`${inter.className} antialiased`}>{children}</body>
+        <body className={`${inter.className} antialiased`}>
+          <TRPCProvider>{children}</TRPCProvider>
+        </body>
       </html>
     </ClerkProvider>
   )
