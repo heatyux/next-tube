a816cf2aaac73ff64b3f5750d8408956b3b2cb92
heatyux <421743974@qq.com>
2025-09-02 11:42:14 +0800

    feat: context and proteced procedure for trpc

--- src/trpc/init.ts
+++ src/trpc/init.ts
@@ -1,19 +1,26 @@
 import { cache } from 'react'
 
-import { initTRPC } from '@trpc/server'
+import { auth } from '@clerk/nextjs/server'
+import { TRPCError, initTRPC } from '@trpc/server'
+import { eq } from 'drizzle-orm'
 import superjson from 'superjson'
 
+import { db } from '@/db'
+import { users } from '@/db/schema'
+
 export const createTRPCContext = cache(async () => {
-  /**
-   * @see: https://trpc.io/docs/server/context
-   */
-  return { userId: 'user_123' }
+  const { userId } = await auth()
+
+  return { clerkUserId: userId }
 })
+
+export type Context = Awaited<ReturnType<typeof createTRPCContext>>
+
 // Avoid exporting the entire t-object
 // since it's not very descriptive.
 // For instance, the use of a t variable
 // is common in i18n libraries.
-const t = initTRPC.create({
+const t = initTRPC.context<Context>().create({
   /**
    * @see https://trpc.io/docs/server/data-transformers
    */
@@ -23,3 +30,36 @@ const t = initTRPC.create({
 export const createTRPCRouter = t.router
 export const createCallerFactory = t.createCallerFactory
 export const baseProcedure = t.procedure
+export const protectedProcedure = t.procedure.use(
+  async function isAuthed(opts) {
+    const { ctx } = opts
+
+    if (!ctx.clerkUserId) {
+      throw new TRPCError({
+        code: 'UNAUTHORIZED',
+        message: 'You must be signed in to do this.',
+      })
+    }
+
+    const user = await db
+      .select()
+      .from(users)
+      .where(eq(users.clerkId, ctx.clerkUserId))
+      .limit(1)
+      .then((res) => res[0])
+
+    if (!user) {
+      throw new TRPCError({
+        code: 'UNAUTHORIZED',
+        message: "User doesn't exist in the database.",
+      })
+    }
+
+    return opts.next({
+      ctx: {
+        ...ctx,
+        user,
+      },
+    })
+  },
+)
