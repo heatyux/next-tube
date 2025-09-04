543e7be71c8ddc74b9593acbeaaba2356f2329cc
heatyux <421743974@qq.com>
2025-09-04 14:10:21 +0800

    feat: remove client component

--- src/app/(home)/client.tsx
+++
@@ -1,9 +0,0 @@
-'use client'
-
-import { trpc } from '@/trpc/client'
-
-export const PageClient = () => {
-  const [data] = trpc.hello.useSuspenseQuery({ text: 'test' })
-
-  return <div>Page Client: {data.greeting}</div>
-}
--- src/app/(home)/page.tsx
+++ src/app/(home)/page.tsx
@@ -1,21 +1,7 @@
-import { Suspense } from 'react'
-
-import { ErrorBoundary } from 'react-error-boundary'
-
 import { HydrateClient, trpc } from '@/trpc/server'
-
-import { PageClient } from './client'
 
 export default function HomePage() {
   void trpc.hello.prefetch({ text: 'ixe' })
 
-  return (
-    <HydrateClient>
-      <Suspense fallback={<p>Loading...</p>}>
-        <ErrorBoundary fallback={<div>Something went wrong.</div>}>
-          <PageClient />
-        </ErrorBoundary>
-      </Suspense>
-    </HydrateClient>
-  )
+  return <HydrateClient></HydrateClient>
 }
