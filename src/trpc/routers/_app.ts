2c8844a1b7f7213a6ad0d6229c474acad2b17e67
heatyux <421743974@qq.com>
2025-09-09 18:09:25 +0800

    feat: video api router registered

--- src/trpc/routers/_app.ts
+++ src/trpc/routers/_app.ts
@@ -1,11 +1,13 @@
 import { categoriesRouter } from '@/modules/categories/server/procedures'
 import { studioRouter } from '@/modules/studio/server/procedures'
+import { videosRouter } from '@/modules/videos/server/procedures'
 
 import { createTRPCRouter } from '../init'
 
 export const appRouter = createTRPCRouter({
   category: categoriesRouter,
   studio: studioRouter,
+  videos: videosRouter,
 })
 // export type definition of API
 export type AppRouter = typeof appRouter
