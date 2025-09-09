4732602c45e0f5ca766c245972853df798aaff1a
heatyux <421743974@qq.com>
2025-09-09 18:32:42 +0800

    feat: suspense and error boundary for page added

--- src/modules/studio/ui/sections/video-section.tsx
+++ src/modules/studio/ui/sections/video-section.tsx
@@ -1,9 +1,31 @@
 'use client'
 
+import { Suspense, useEffect, useState } from 'react'
+
+import { ErrorBoundary } from 'react-error-boundary'
+
 import { DEFAULT_LIMIT } from '@/constants'
 import { trpc } from '@/trpc/client'
 
-export const VideoSection = () => {
+export const VideosSection = () => {
+  const [isMounted, setIsMounted] = useState(false)
+
+  useEffect(() => {
+    setIsMounted(true)
+  }, [])
+
+  if (!isMounted) return null
+
+  return (
+    <Suspense fallback={<p>Loading...</p>}>
+      <ErrorBoundary fallback={<p>Error</p>}>
+        <VideosSectionSuspense />
+      </ErrorBoundary>
+    </Suspense>
+  )
+}
+
+const VideosSectionSuspense = () => {
   const [data] = trpc.studio.getMany.useSuspenseInfiniteQuery(
     {
       limit: DEFAULT_LIMIT,
