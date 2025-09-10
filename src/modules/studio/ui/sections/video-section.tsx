9168c2a68cc86ce193e78d8475c14d1833896a11
heatyux <421743974@qq.com>
2025-09-10 11:41:52 +0800

    feat: render table with videos and infinite load data

--- src/modules/studio/ui/sections/video-section.tsx
+++ src/modules/studio/ui/sections/video-section.tsx
@@ -2,8 +2,18 @@
 
 import { Suspense, useEffect, useState } from 'react'
 
+import { useRouter } from 'next/navigation'
 import { ErrorBoundary } from 'react-error-boundary'
 
+import { InfiniteScroll } from '@/components/infinite-scroll'
+import {
+  Table,
+  TableBody,
+  TableCell,
+  TableHead,
+  TableHeader,
+  TableRow,
+} from '@/components/ui/table'
 import { DEFAULT_LIMIT } from '@/constants'
 import { trpc } from '@/trpc/client'
 
@@ -26,7 +36,9 @@ export const VideosSection = () => {
 }
 
 const VideosSectionSuspense = () => {
-  const [data] = trpc.studio.getMany.useSuspenseInfiniteQuery(
+  const router = useRouter()
+
+  const [videos, query] = trpc.studio.getMany.useSuspenseInfiniteQuery(
     {
       limit: DEFAULT_LIMIT,
     },
@@ -35,5 +47,48 @@ const VideosSectionSuspense = () => {
     },
   )
 
-  return <div>{JSON.stringify(data)}</div>
+  return (
+    <div className="h-[200px]">
+      <div>
+        <Table>
+          <TableHeader>
+            <TableRow>
+              <TableHead className="w-[510px] pl-6">Video</TableHead>
+              <TableHead>Visibility</TableHead>
+              <TableHead>Status</TableHead>
+              <TableHead>Date</TableHead>
+              <TableHead className="text-right">Views</TableHead>
+              <TableHead className="text-right">Comments</TableHead>
+              <TableHead className="pr-6 text-right">Likes</TableHead>
+            </TableRow>
+          </TableHeader>
+          <TableBody>
+            {videos.pages
+              .flatMap((page) => page.items)
+              .map((video) => (
+                <TableRow
+                  key={video.id}
+                  onClick={() => router.push(`/studio/videos/${video.id}`)}
+                  className="cursor-pointer"
+                >
+                  <TableCell>{video.title}</TableCell>
+                  <TableCell>Visibility</TableCell>
+                  <TableCell>Status</TableCell>
+                  <TableCell>Date</TableCell>
+                  <TableCell>Views</TableCell>
+                  <TableCell>Comments</TableCell>
+                  <TableCell>Likes</TableCell>
+                </TableRow>
+              ))}
+          </TableBody>
+        </Table>
+      </div>
+      <InfiniteScroll
+        isManual
+        hasNextPage={query.hasNextPage}
+        isFetchingNextPage={query.isFetchingNextPage}
+        fetchNextPage={query.fetchNextPage}
+      />
+    </div>
+  )
 }
