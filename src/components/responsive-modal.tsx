dc0c4b012ec635a59850ef93ccdcb36e2bbc73eb
heatyux <421743974@qq.com>
2025-09-10 12:31:31 +0800

    feat: reponsive modal component implemented

---
+++ src/components/responsive-modal.tsx
@@ -0,0 +1,54 @@
+import type { PropsWithChildren } from 'react'
+
+import {
+  Dialog,
+  DialogContent,
+  DialogHeader,
+  DialogTitle,
+} from '@/components/ui/dialog'
+import {
+  Drawer,
+  DrawerContent,
+  DrawerHeader,
+  DrawerTitle,
+} from '@/components/ui/drawer'
+import { useIsMobile } from '@/hooks/use-mobile'
+
+interface ResponsiveModalProps {
+  title: string
+  open: boolean
+  onOpenChange: (open: boolean) => void
+}
+
+export const ResponsiveModal = ({
+  children,
+  title,
+  open,
+  onOpenChange,
+}: PropsWithChildren<ResponsiveModalProps>) => {
+  const isMobile = useIsMobile()
+
+  if (isMobile) {
+    return (
+      <Drawer open={open} onOpenChange={onOpenChange}>
+        <DrawerContent>
+          <DrawerHeader>
+            <DrawerTitle>{title}</DrawerTitle>
+          </DrawerHeader>
+          {children}
+        </DrawerContent>
+      </Drawer>
+    )
+  }
+
+  return (
+    <Dialog open={open} onOpenChange={onOpenChange}>
+      <DialogContent>
+        <DialogHeader>
+          <DialogTitle>{title}</DialogTitle>
+        </DialogHeader>
+        {children}
+      </DialogContent>
+    </Dialog>
+  )
+}
--- src/modules/studio/ui/components/studio-upload-modal.tsx
+++ src/modules/studio/ui/components/studio-upload-modal.tsx
@@ -3,6 +3,7 @@
 import { Loader2Icon, PlusIcon } from 'lucide-react'
 import { toast } from 'sonner'
 
+import { ResponsiveModal } from '@/components/responsive-modal'
 import { Button } from '@/components/ui/button'
 import { trpc } from '@/trpc/client'
 
@@ -19,17 +20,26 @@ export const StudioUploadModal = () => {
   })
 
   return (
-    <Button
-      disabled={create.isPending}
-      variant="secondary"
-      onClick={() => create.mutate()}
-    >
-      {create.isPending ? (
-        <Loader2Icon className="text-muted-foreground size-5 animate-spin" />
-      ) : (
-        <PlusIcon className="mr-2" />
-      )}
-      <span>Create</span>
-    </Button>
+    <>
+      <Button
+        disabled={create.isPending}
+        variant="secondary"
+        onClick={() => create.mutate()}
+      >
+        {create.isPending ? (
+          <Loader2Icon className="text-muted-foreground size-5 animate-spin" />
+        ) : (
+          <PlusIcon className="mr-2" />
+        )}
+        <span>Create</span>
+      </Button>
+      <ResponsiveModal
+        title="Upload a video"
+        open={!!create.data}
+        onOpenChange={() => create.reset()}
+      >
+        <p>This will be an upload</p>
+      </ResponsiveModal>
+    </>
   )
 }
