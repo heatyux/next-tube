518eefa737464912a34b4d5e5e8769b30e2364f7
heatyux <421743974@qq.com>
2025-09-11 12:12:16 +0800

    feat: mux uploader setup in procedure

--- src/modules/videos/server/procedures.ts
+++ src/modules/videos/server/procedures.ts
@@ -1,10 +1,19 @@
 import { db } from '@/db'
 import { videos } from '@/db/schema'
+import { mux } from '@/lib/mux'
 import { createTRPCRouter, protectedProcedure } from '@/trpc/init'
 
 export const videosRouter = createTRPCRouter({
   create: protectedProcedure.mutation(async ({ ctx }) => {
     const { id: userId } = ctx.user
+
+    const upload = await mux.video.uploads.create({
+      new_asset_settings: {
+        passthrough: userId,
+        playback_policies: ['public'],
+      },
+      cors_origin: '*', // TODO: In production, set to the application's url
+    })
 
     const [video] = await db
       .insert(videos)
@@ -14,6 +23,6 @@ export const videosRouter = createTRPCRouter({
       })
       .returning()
 
-    return { video }
+    return { video, url: upload.url }
   }),
 })
