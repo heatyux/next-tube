15a31f9c689c170da23b0be7fdd8783e68993401
heatyux <421743974@qq.com>
2025-09-12 12:14:45 +0800

    feat: get video by id api created

--- src/modules/studio/server/procedures.ts
+++ src/modules/studio/server/procedures.ts
@@ -1,3 +1,4 @@
+import { TRPCError } from '@trpc/server'
 import { and, desc, eq, lt, or } from 'drizzle-orm'
 import { z } from 'zod'
 
@@ -6,6 +7,23 @@ import { videos } from '@/db/schema'
 import { createTRPCRouter, protectedProcedure } from '@/trpc/init'
 
 export const studioRouter = createTRPCRouter({
+  getOne: protectedProcedure
+    .input(z.object({ id: z.uuid() }))
+    .query(async ({ ctx, input }) => {
+      const { id: userId } = ctx.user
+      const { id } = input
+
+      const [video] = await db
+        .select()
+        .from(videos)
+        .where(and(eq(videos.id, id), eq(videos.userId, userId)))
+
+      if (!video) {
+        throw new TRPCError({ code: 'BAD_REQUEST', message: 'Video not found' })
+      }
+
+      return video
+    }),
   getMany: protectedProcedure
     .input(
       z.object({
