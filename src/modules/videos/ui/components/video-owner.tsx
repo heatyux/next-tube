841ec853374399dd1492b4883a9d4f062117881a
heatyux <421743974@qq.com>
2025-09-29 11:21:34 +0800

    feat: user info for video page implemented

--- src/modules/videos/ui/components/video-owner.tsx
+++ src/modules/videos/ui/components/video-owner.tsx
@@ -6,6 +6,7 @@ import { Button } from '@/components/ui/button'
 import { UserAvatar } from '@/components/user-avatar'
 import { userSelectSchema } from '@/db/schema'
 import { SubscriptionButton } from '@/modules/subscriptions/ui/components/subscription-button'
+import { UserInfo } from '@/modules/users/ui/components/user-info'
 
 interface VideoOwnerProps {
   user: z.infer<typeof userSelectSchema>
@@ -20,10 +21,13 @@ export const VideoOwner = ({ user, videoId }: VideoOwnerProps) => {
       <Link href={`/users/${user.id}`}>
         <div className="flex min-w-0 items-center gap-3">
           <UserAvatar imageUrl={user.imageUrl} name={user.name} size="lg" />
-          <span className="text-muted-foreground line-clamp-1 text-sm">
-            {/* TODO: Properly fill in the subscribers */}
-            {0} Subscribers
-          </span>
+          <div className="flex min-w-0 flex-col gap-1">
+            <UserInfo size="default" name={user.name} />
+            <span className="text-muted-foreground line-clamp-1 text-sm">
+              {/* TODO: Properly fill in the subscribers */}
+              {0} Subscribers
+            </span>
+          </div>
         </div>
       </Link>
       {userId === user.clerkId ? (
