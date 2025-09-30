506ea6e9306a74c0292fd78bce51b45eff402954
heatyux <421743974@qq.com>
2025-09-30 10:14:32 +0800

    feat: video view router registered

--- src/trpc/routers/_app.ts
+++ src/trpc/routers/_app.ts
@@ -1,5 +1,6 @@
 import { categoriesRouter } from '@/modules/categories/server/procedures'
 import { studioRouter } from '@/modules/studio/server/procedures'
+import { videoViewsRouter } from '@/modules/video-views/server/procedures'
 import { videosRouter } from '@/modules/videos/server/procedures'
 
 import { createTRPCRouter } from '../init'
@@ -8,6 +9,7 @@ export const appRouter = createTRPCRouter({
   category: categoriesRouter,
   studio: studioRouter,
   videos: videosRouter,
+  videoViews: videoViewsRouter,
 })
 // export type definition of API
 export type AppRouter = typeof appRouter
