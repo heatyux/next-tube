d1bdff1328f2c33d24cf3574117397a38eaf702e
heatyux <421743974@qq.com>
2025-10-13 12:05:09 +0800

    feat: schema for video subscriptions  created

--- src/db/schema.ts
+++ src/db/schema.ts
@@ -34,6 +34,12 @@ export const userRelations = relations(users, ({ many }) => ({
   videos: many(videos),
   videoView: many(videoViews),
   videoReaction: many(videoReactions),
+  subscriptions: many(subscriptions, {
+    relationName: 'subscriptions_viewer_id_fk',
+  }),
+  subscribers: many(subscriptions, {
+    relationName: 'subscriptions_creator_id_fk',
+  }),
 }))
 
 export const userSelectSchema = createSelectSchema(users)
@@ -167,3 +173,40 @@ export const videoReactionsRelations = relations(videoReactions, ({ one }) => ({
 export const videoReactionsInsertSchema = createInsertSchema(videoReactions)
 export const videoReactionsSelectSchema = createSelectSchema(videoReactions)
 export const videoReactionsUpdateSchema = createUpdateSchema(videoReactions)
+
+export const subscriptions = pgTable(
+  'subscriptions',
+  {
+    viewerId: uuid('viewer_id')
+      .references(() => users.id, {
+        onDelete: 'cascade',
+      })
+      .notNull(),
+    creatorId: uuid('creator_id')
+      .references(() => users.id, {
+        onDelete: 'cascade',
+      })
+      .notNull(),
+    createdAt: timestamp('created_at').defaultNow().notNull(),
+    updatedAt: timestamp('updated_at').defaultNow().notNull(),
+  },
+  (t) => [
+    primaryKey({
+      name: 'pk_subscriptions',
+      columns: [t.viewerId, t.creatorId],
+    }),
+  ],
+)
+
+export const subscriptionsRelations = relations(subscriptions, ({ one }) => ({
+  viewerId: one(users, {
+    fields: [subscriptions.viewerId],
+    references: [users.id],
+    relationName: 'subscriptions_viewer_id_fk',
+  }),
+  creatorId: one(users, {
+    fields: [subscriptions.creatorId],
+    references: [users.id],
+    relationName: 'subscriptions_creator_id_fk',
+  }),
+}))
