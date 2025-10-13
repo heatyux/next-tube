285ffa0090194a70d2fb33f66752e25214923da1
heatyux <421743974@qq.com>
2025-10-13 12:15:24 +0800

    feat: subscriptions router created

---
+++ src/modules/subscriptions/server/procedures.ts
@@ -0,0 +1,28 @@
+import { TRPCError } from '@trpc/server'
+import z from 'zod'
+
+import { db } from '@/db'
+import { subscriptions } from '@/db/schema'
+import { createTRPCRouter, protectedProcedure } from '@/trpc/init'
+
+export const subscriptionsRouter = createTRPCRouter({
+  create: protectedProcedure
+    .input(z.object({ userId: z.uuid() }))
+    .mutation(async ({ ctx, input }) => {
+      const { userId } = input
+
+      if (userId === ctx.user.id) {
+        throw new TRPCError({
+          code: 'BAD_REQUEST',
+          message: 'You cannot subscribe to yourself',
+        })
+      }
+
+      const [createdSubscription] = await db
+        .insert(subscriptions)
+        .values({ viewerId: ctx.user.id, creatorId: userId })
+        .returning()
+
+      return createdSubscription
+    }),
+})
