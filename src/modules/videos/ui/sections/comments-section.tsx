bc5f76248e34a86278306a21568f0626fe46005a
heatyux <421743974@qq.com>
2025-10-15 12:12:26 +0800

    feat: comments section skeleton implemented

--- src/modules/videos/ui/sections/comments-section.tsx
+++ src/modules/videos/ui/sections/comments-section.tsx
@@ -2,6 +2,7 @@
 
 import { Suspense } from 'react'
 
+import { Loader2Icon } from 'lucide-react'
 import { ErrorBoundary } from 'react-error-boundary'
 
 import { InfiniteScroll } from '@/components/infinite-scroll'
@@ -16,11 +17,19 @@ interface CommentsSectionProps {
 
 export const CommentsSection = ({ videoId }: CommentsSectionProps) => {
   return (
-    <Suspense fallback={<p>Loading...</p>}>
+    <Suspense fallback={<CommentsSectionSkeleton />}>
       <ErrorBoundary fallback={<p>Error</p>}>
         <CommentsSectionSuspense videoId={videoId} />
       </ErrorBoundary>
     </Suspense>
+  )
+}
+
+const CommentsSectionSkeleton = () => {
+  return (
+    <div className="mt-6 flex items-center justify-center">
+      <Loader2Icon className="text-muted-foreground animete-spin size-7" />
+    </div>
   )
 }
 
