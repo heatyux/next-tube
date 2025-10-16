95f3c95b7bae15c1bb8dfe2c64fdc9f10e2ff9a6
heatyux <421743974@qq.com>
2025-10-16 14:37:46 +0800

    feat: comment reaction api created

---
+++ src/modules/comment-reactions/server/procedures.ts
@@ -0,0 +1,115 @@
+import { and, eq } from 'drizzle-orm'
+import z from 'zod'
+
+import { db } from '@/db'
+import { commentReactions } from '@/db/schema'
+import { createTRPCRouter, protectedProcedure } from '@/trpc/init'
+
+export const commentReactionsRouter = createTRPCRouter({
+  like: protectedProcedure
+    .input(
+      z.object({
+        commentId: z.uuid(),
+      }),
+    )
+    .mutation(async ({ ctx, input }) => {
+      const { id: userId } = ctx.user
+      const { commentId } = input
+
+      const [existingCommentReactionLike] = await db
+        .select()
+        .from(commentReactions)
+        .where(
+          and(
+            eq(commentReactions.commentId, commentId),
+            eq(commentReactions.userId, userId),
+            eq(commentReactions.type, 'like'),
+          ),
+        )
+
+      if (existingCommentReactionLike) {
+        const [deletedCommentReaction] = await db
+          .delete(commentReactions)
+          .where(
+            and(
+              eq(
+                commentReactions.commentId,
+                eq(commentReactions.userId, userId),
+              ),
+            ),
+          )
+          .returning()
+
+        return deletedCommentReaction
+      }
+
+      const [createdCommentReactionLike] = await db
+        .insert(commentReactions)
+        .values({
+          userId,
+          commentId,
+          type: 'like',
+        })
+        .onConflictDoUpdate({
+          target: [commentReactions.commentId, commentReactions.userId],
+          set: {
+            type: 'like',
+          },
+        })
+        .returning()
+
+      return createdCommentReactionLike
+    }),
+  dislike: protectedProcedure
+    .input(
+      z.object({
+        commentId: z.uuid(),
+      }),
+    )
+    .mutation(async ({ ctx, input }) => {
+      const { id: userId } = ctx.user
+      const { commentId } = input
+
+      const [existingCommentReactionDislike] = await db
+        .select()
+        .from(commentReactions)
+        .where(
+          and(
+            eq(commentReactions.commentId, commentReactions.commentId),
+            eq(commentReactions.userId, commentReactions.userId),
+            eq(commentReactions.type, 'dislike'),
+          ),
+        )
+
+      if (existingCommentReactionDislike) {
+        const [deletedCommentReactionDislike] = await db
+          .delete(commentReactions)
+          .where(
+            and(
+              eq(commentReactions.commentId, commentId),
+              eq(commentReactions.userId, userId),
+            ),
+          )
+          .returning()
+
+        return deletedCommentReactionDislike
+      }
+
+      const [createdCommentReactionDislike] = await db
+        .insert(commentReactions)
+        .values({
+          commentId,
+          userId,
+          type: 'dislike',
+        })
+        .onConflictDoUpdate({
+          target: [commentReactions.commentId, commentReactions.userId],
+          set: {
+            type: 'dislike',
+          },
+        })
+        .returning()
+
+      return createdCommentReactionDislike
+    }),
+})
