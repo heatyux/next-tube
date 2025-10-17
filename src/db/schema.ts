f9dd834d85ac5578fa38e7d4a7a170e28702001d
heatyux <421743974@qq.com>
2025-10-17 15:30:10 +0800

    feat: comment for parent shceme added

--- src/db/schema.ts
+++ src/db/schema.ts
@@ -1,5 +1,6 @@
 import { relations } from 'drizzle-orm'
 import {
+  foreignKey,
   integer,
   pgEnum,
   pgTable,
@@ -214,18 +215,31 @@ export const subscriptionsRelations = relations(subscriptions, ({ one }) => ({
   }),
 }))
 
-export const comments = pgTable('comments', {
-  id: uuid('id').defaultRandom().primaryKey(),
-  userId: uuid('user_id')
-    .references(() => users.id, { onDelete: 'cascade' })
-    .notNull(),
-  videoId: uuid('video_id')
-    .references(() => videos.id, { onDelete: 'cascade' })
-    .notNull(),
-  value: text('value').notNull(),
-  createdAt: timestamp('created_at').defaultNow().notNull(),
-  updatedAt: timestamp('updated_at').defaultNow().notNull(),
-})
+export const comments = pgTable(
+  'comments',
+  {
+    id: uuid('id').defaultRandom().primaryKey(),
+    parentId: uuid('parent_id'),
+    userId: uuid('user_id')
+      .references(() => users.id, { onDelete: 'cascade' })
+      .notNull(),
+    videoId: uuid('video_id')
+      .references(() => videos.id, { onDelete: 'cascade' })
+      .notNull(),
+    value: text('value').notNull(),
+    createdAt: timestamp('created_at').defaultNow().notNull(),
+    updatedAt: timestamp('updated_at').defaultNow().notNull(),
+  },
+  (t) => {
+    return [
+      foreignKey({
+        columns: [t.parentId],
+        foreignColumns: [t.id],
+        name: 'fk_comments_parent_id',
+      }).onDelete('cascade'),
+    ]
+  },
+)
 
 export const commentsRelations = relations(comments, ({ one, many }) => ({
   user: one(users, {
@@ -236,7 +250,15 @@ export const commentsRelations = relations(comments, ({ one, many }) => ({
     fields: [comments.videoId],
     references: [videos.id],
   }),
+  parent: one(comments, {
+    fields: [comments.parentId],
+    references: [comments.id],
+    relationName: 'fk_comments_parent_id',
+  }),
   commentReactions: many(commentReactions),
+  replies: many(comments, {
+    relationName: 'fk_comments_parent_id',
+  }),
 }))
 
 export const commentsSelectSchema = createSelectSchema(comments)
