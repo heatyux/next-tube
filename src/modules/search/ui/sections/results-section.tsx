5924797372fbccf302c252c50f45e855ac34902e
heatyux <421743974@qq.com>
2025-10-29 12:17:55 +0800

    feat: result section implemented

--- src/modules/search/ui/sections/results-section.tsx
+++ src/modules/search/ui/sections/results-section.tsx
@@ -1,8 +1,95 @@
+'use client'
+
+import { Suspense } from 'react'
+
+import { ErrorBoundary } from 'react-error-boundary'
+
+import { InfiniteScroll } from '@/components/infinite-scroll'
+import { DEFAULT_LIMIT } from '@/constants'
+import { useIsMobile } from '@/hooks/use-mobile'
+import {
+  VideoGridCard,
+  VideoGridCardSkeleton,
+} from '@/modules/videos/ui/components/video-grid-card'
+import {
+  VideoRowCard,
+  VideoRowCardSkeleton,
+} from '@/modules/videos/ui/components/video-row-card'
+import { trpc } from '@/trpc/client'
+
 interface ResultsSectionProps {
   query?: string
   categoryId?: string
 }
 
-export const ResultsSection = ({}: ResultsSectionProps) => {
-  return <div></div>
+export const ResultsSection = ({ query, categoryId }: ResultsSectionProps) => {
+  return (
+    <Suspense fallback={<ResultsSectionSkeleton />}>
+      <ErrorBoundary fallback={<p>Error</p>}>
+        <ResultsSectionSuspense query={query} categoryId={categoryId} />
+      </ErrorBoundary>
+    </Suspense>
+  )
+}
+
+export const ResultsSectionSkeleton = () => {
+  return (
+    <div>
+      <div className="hidden flex-col gap-4 md:flex">
+        {Array.from({ length: 6 }).map((_, index) => (
+          <VideoRowCardSkeleton key={index} />
+        ))}
+      </div>
+      <div className="flex flex-col gap-4 gap-y-10 p-4 pt-6 md:hidden">
+        {Array.from({ length: 5 }).map((_, index) => (
+          <VideoGridCardSkeleton key={index} />
+        ))}
+      </div>
+    </div>
+  )
+}
+
+export const ResultsSectionSuspense = ({
+  query,
+  categoryId,
+}: ResultsSectionProps) => {
+  const isMobile = useIsMobile()
+
+  const [results, resultQuery] = trpc.search.getMany.useSuspenseInfiniteQuery(
+    {
+      query,
+      categoryId,
+      limit: DEFAULT_LIMIT,
+    },
+    {
+      getNextPageParam: (lastPage) => lastPage.nextCursor,
+    },
+  )
+
+  return (
+    <>
+      {isMobile ? (
+        <div className="flex flex-col gap-4 gap-y-10">
+          {results.pages
+            .flatMap((page) => page.items)
+            .map((video) => (
+              <VideoGridCard key={video.id} data={video} />
+            ))}
+        </div>
+      ) : (
+        <div className="flex flex-col gap-4">
+          {results.pages
+            .flatMap((page) => page.items)
+            .map((video) => (
+              <VideoRowCard key={video.id} data={video} />
+            ))}
+        </div>
+      )}
+      <InfiniteScroll
+        hasNextPage={resultQuery.hasNextPage}
+        isFetchingNextPage={resultQuery.isFetchingNextPage}
+        fetchNextPage={resultQuery.fetchNextPage}
+      />
+    </>
+  )
 }
