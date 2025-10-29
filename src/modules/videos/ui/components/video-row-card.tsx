356fcf2b36c54d02007fa7c538d6a997d43eb661
heatyux <421743974@qq.com>
2025-10-29 12:01:33 +0800

    feat: video row card default props

--- src/modules/videos/ui/components/video-row-card.tsx
+++ src/modules/videos/ui/components/video-row-card.tsx
@@ -47,7 +47,7 @@ interface VideoRowCardProps extends VariantProps<typeof videoRowCardVariants> {
 }
 
 export const VideoRowCardSkeleton = ({
-  size,
+  size = 'default',
 }: VariantProps<typeof videoRowCardVariants>) => {
   return (
     <div className={videoRowCardVariants({ size })}>
@@ -78,7 +78,11 @@ export const VideoRowCardSkeleton = ({
   )
 }
 
-export const VideoRowCard = ({ data, size, onRemove }: VideoRowCardProps) => {
+export const VideoRowCard = ({
+  data,
+  size = 'default',
+  onRemove,
+}: VideoRowCardProps) => {
   const compactViews = useMemo(
     () => Intl.NumberFormat('en-US').format(data.viewCount),
     [data.viewCount],
