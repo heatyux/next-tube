df91e1a39505d25a4ff9717fa51121a5f763f809
heatyux <421743974@qq.com>
2025-10-30 15:56:51 +0800

    feat: home page videos section added

--- src/app/(home)/page.tsx
+++ src/app/(home)/page.tsx
@@ -1,3 +1,4 @@
+import { DEFAULT_LIMIT } from '@/constants'
 import { HomeView } from '@/modules/home/ui/views/home-view'
 import { HydrateClient, trpc } from '@/trpc/server'
 
@@ -13,6 +14,10 @@ export default async function HomePage({ searchParams }: HomePageProps) {
   const { categoryId } = await searchParams
 
   void trpc.category.getMany.prefetch()
+  void trpc.videos.getMany.prefetchInfinite({
+    categoryId,
+    limit: DEFAULT_LIMIT,
+  })
 
   return (
     <HydrateClient>
