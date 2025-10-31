93c1b4f09d45fa573eac32f53ba7eada0e852df2
heatyux <421743974@qq.com>
2025-10-31 14:43:45 +0800

    feat: invalidate liked playlists for video reaction implemented

--- src/modules/videos/ui/components/video-reactions.tsx
+++ src/modules/videos/ui/components/video-reactions.tsx
@@ -29,7 +29,7 @@ export const VideoReactions = ({
   const like = trpc.videoReactions.like.useMutation({
     onSuccess: () => {
       utils.videos.getOne.invalidate({ id: videoId })
-      // TODO: Invalidate 'liked' playlists
+      utils.playlists.getLiked.invalidate()
     },
     onError: (error) => {
       toast.error('Something went wrong')
@@ -43,7 +43,7 @@ export const VideoReactions = ({
   const dislike = trpc.videoReactions.dislike.useMutation({
     onSuccess: () => {
       utils.videos.getOne.invalidate({ id: videoId })
-      // TODO: Invalidate 'disliked' playlists
+      utils.playlists.getLiked.invalidate()
     },
     onError: (error) => {
       toast.error('Something went wrong')
