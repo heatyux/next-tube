028c5557e82a252c8f25a5cd596451aea7f34030
heatyux <421743974@qq.com>
2025-11-03 10:47:58 +0800

    feat: playlist skeleton implemented

--- src/modules/playlists/components/playlist-grid-card/index.tsx
+++ src/modules/playlists/components/playlist-grid-card/index.tsx
@@ -3,11 +3,25 @@ import Link from 'next/link'
 import { THUMBNAIL_FALLBACK } from '@/modules/videos/constants'
 
 import { PlaylistGetManyOutput } from '../../types'
-import { PlaylistThumbnail } from './playlist-thumbnail'
+import { PlaylistInfo, PlaylistInfoSkeleton } from './playlist-info'
+import {
+  PlaylistThumbnail,
+  PlaylistThumbnailSkeleton,
+} from './playlist-thumbnail'
 
 interface PlaylistGridCardProps {
   data: PlaylistGetManyOutput['items'][number]
 }
+
+export const PlaylistGridCardSkeleton = () => {
+  return (
+    <div className="flex w-full flex-col gap-2">
+      <PlaylistThumbnailSkeleton />
+      <PlaylistInfoSkeleton />
+    </div>
+  )
+}
+
 export const PlaylistGridCard = ({ data }: PlaylistGridCardProps) => {
   return (
     <Link href={`/playlists/${data.id}`}>
@@ -17,6 +31,7 @@ export const PlaylistGridCard = ({ data }: PlaylistGridCardProps) => {
           videoCount={data.playlistVideoCount}
           imageUrl={THUMBNAIL_FALLBACK}
         />
+        <PlaylistInfo data={data} />
       </div>
     </Link>
   )
--- src/modules/playlists/ui/sections/playlists-section.tsx
+++ src/modules/playlists/ui/sections/playlists-section.tsx
@@ -6,11 +6,12 @@ import { ErrorBoundary } from 'react-error-boundary'
 
 import { InfiniteScroll } from '@/components/infinite-scroll'
 import { DEFAULT_LIMIT } from '@/constants'
-import { VideoGridCardSkeleton } from '@/modules/videos/ui/components/video-grid-card'
-import { VideoRowCardSkeleton } from '@/modules/videos/ui/components/video-row-card'
 import { trpc } from '@/trpc/client'
 
-import { PlaylistGridCard } from '../../components/playlist-grid-card'
+import {
+  PlaylistGridCard,
+  PlaylistGridCardSkeleton,
+} from '../../components/playlist-grid-card'
 
 export const PlaylistsSection = () => {
   return (
@@ -24,17 +25,10 @@ export const PlaylistsSection = () => {
 
 const PlaylistsSectionSkeleton = () => {
   return (
-    <div>
-      <div className="flex flex-col gap-4 gap-y-10 md:hidden">
-        {Array.from({ length: 20 }).map((_, index) => (
-          <VideoGridCardSkeleton key={index} />
-        ))}
-      </div>
-      <div className="hidden flex-col gap-4 md:flex">
-        {Array.from({ length: 20 }).map((_, index) => (
-          <VideoRowCardSkeleton key={index} size="compact" />
-        ))}
-      </div>
+    <div className="grid grid-cols-1 gap-4 gap-y-10 sm:grid-cols-2 lg:grid-cols-3 xl:grid-cols-3 2xl:grid-cols-4 [@media(min-width:1920px)]:grid-cols-5 [@media(min-width:2200px)]:grid-cols-6">
+      {Array.from({ length: 20 }).map((_, index) => (
+        <PlaylistGridCardSkeleton key={index} />
+      ))}
     </div>
   )
 }
